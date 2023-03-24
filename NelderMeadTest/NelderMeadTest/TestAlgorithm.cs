b6e376d7839d6132acab1ab8c474490b803e950e
destroY17 <105844351+destroY17@users.noreply.github.com>
2023-03-24 22:27:16 +0300

    Fix tests

--- NelderMeadTest/NelderMeadTest/TestAlgorithm.cs
+++ NelderMeadTest/NelderMeadTest/TestAlgorithm.cs
@@ -1,18 +1,10 @@
 using NelderMeadMethod;
-using System.Security.Cryptography.X509Certificates;
 
 namespace NelderMeadTest
 {
     [TestClass]
     public class TestAlgorithm
     {
-        private class Cube : IFunction
-        {
-            public int Dimension => 2;
-
-            public double Calculate(Point p) => 5 * Math.Sin(2 * p[0]) + Math.Pow(p[0], 2);
-        }
-
         private NelderMead _algorithm = new NelderMead();
         private IFunction _rosenbrock = new RosenbrockFunction();
         private IFunction _himmelblaus = new HimmelblausFunction();
@@ -53,17 +45,6 @@ namespace NelderMeadTest
             var initialPoint = new Point(-5, 10);
             var answer = _himmelblaus.Calculate(_algorithm.Run(_himmelblaus, initialPoint));
             var expected = _himmelblaus.Calculate(new Point(3, 2));
-
-            Assert.IsTrue((answer - expected) < 0.001);
-        }
-
-        [TestMethod]
-        public void DoesNotFindOptimum()
-        {
-            var initialPoint = new Point(5, 5);
-            var cubeFunction = new Cube();
-            var answer = cubeFunction.Calculate(_algorithm.Run(cubeFunction, initialPoint));
-            var expected = cubeFunction.Calculate(new Point(0, 0));
 
             Assert.IsTrue((answer - expected) < 0.001);
         }
